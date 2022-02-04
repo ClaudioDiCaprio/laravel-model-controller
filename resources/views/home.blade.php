d4b79b4f1c2b2ed1d6ef385fda381b5502b3cfe5
Claudio <claudiodicaprio89@gmail.com>
2022-02-04 15:45:18 +0100

    movie::all()

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,14 @@
-<h1>Home</h1>
+{{-- @dd($movies) --}}
+
+<ul>
+    @foreach ($movies as $movie)
+        <li>
+            Title: {{$movie->title}},
+            Original_Title {{$movie->original_title}},
+            Nationality: {{$movie->nationality}},
+            Release Date: {{$movie->date}},
+            Vote: {{$movie->vote}}.
+        </li>
+        <div>.</div>
+    @endforeach
+</ul>
