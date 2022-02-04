83263413bdb18c71bcec84abca6eb9aaacec95b5
Claudio <claudiodicaprio89@gmail.com>
2022-02-04 15:54:43 +0100

    orderBy

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -9,9 +9,17 @@ class MovieController extends Controller
 {
     public function index()
     {
+
+        //SELECT ALL
+        
         // $movies = Movie::all();
+
         //SELECT * FROM movie WHERE vote >= 8;
-        $movies = Movie::where("vote",">=", 8)->get();
+
+        // $movies = Movie::where("vote",">=", 8)->get();
+
+        //ORDER BY DESC
+        $movies = Movie::where("vote",">=", 8)->orderBy("vote","desc")->get();
         // dd($movies);
         return view ("home",compact("movies"));
     }
